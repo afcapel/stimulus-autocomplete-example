94a1d6fff2b082900759fb04df0b776a0ff7a70b
Alberto Fernandez-Capel <afcapel@gmail.com>
2021-01-18 17:04:05 +0000

    Remove trace

--- app.js
+++ app.js
@@ -2,6 +2,4 @@ import { Application } from 'stimulus'
 import { Autocomplete } from 'stimulus-autocomplete'
 
 const application = Application.start()
-
-console.log("Autocomplete", Autocomplete)
 application.register('autocomplete', Autocomplete)
